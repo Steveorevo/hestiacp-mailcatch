9d04468bdb8bc70aedcce438066deab55b7094db
Stephen J Carnam <steveorevo@gmail.com>
2023-03-21 12:50:41 -0700

    wip

--- nodeapp/debuglog.js
+++ nodeapp/debuglog.js
@@ -2,6 +2,23 @@ const debuglog = module.exports = {}
 debuglog.log = function(data) {
     console.log(data);
     const fs = require('fs');
-    data = JSON.stringify(data, null, 2);
+
+    function stringify(obj) {
+        let cache = [];
+        let str = JSON.stringify(obj, function(key, value) {
+          if (typeof value === "object" && value !== null) {
+            if (cache.indexOf(value) !== -1) {
+              // Circular reference found, discard key
+              return;
+            }
+            // Store value in our collection
+            cache.push(value);
+          }
+          return value;
+        }, 2);
+        cache = null; // reset the cache
+        return str;
+    }
+    data = stringify(data, null, 2);
     fs.appendFileSync('/tmp/debuglog.log', data + "\n", 'utf8');
 }
