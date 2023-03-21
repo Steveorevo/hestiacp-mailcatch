30c72bd13b3aeb9ac7c30e267aaf4180696d935a
Stephen J Carnam <steveorevo@gmail.com>
2023-03-21 12:48:15 -0700

    wip

--- nodeapp/debuglog.js
+++ nodeapp/debuglog.js
@@ -1,5 +1,6 @@
 const debuglog = module.exports = {}
 debuglog.log = function(data) {
+    console.log(data);
     const fs = require('fs');
     data = JSON.stringify(data, null, 2);
     fs.appendFileSync('/tmp/debuglog.log', data + "\n", 'utf8');
