d71bfbd177b7ca89aff0a04b62bed0a53f61fb48
Stephen J Carnam <steveorevo@gmail.com>
2023-03-25 11:28:40 -0700

    resolve domain by path

--- src/lib/catchmail.js
+++ src/lib/catchmail.js
@@ -35,15 +35,50 @@ function CatchMail() {
 
     // Find domain's SMTP credentials
     const fs = require('fs');
-    let mailcatcher_domain = process.env.MAILCATCHER_DOMAIN; // TODO: guess domain for nodejs processes?
+    let mailcatcher_domain = process.env.MAILCATCHER_DOMAIN;
     let mailcatcher_pw = '';
-    const smtp_file = '/home/' + process.env.USER + '/web/' + mailcatcher_domain + '/private/smtp.json';
+    let smtp_file = '/home/' + process.env.USER + '/web/' + mailcatcher_domain + '/private/smtp.json';
     if ( fs.existsSync(smtp_file) ) {
         let smtp_cred = JSON.parse(fs.readFileSync(smtp_file, 'utf8'));
         mailcatcher_domain = smtp_cred.username;
         mailcatcher_pw = smtp_cred.password;
     }else{
-        console.log('SMTP credentials file not found: ' + smtp_file + ' is MAILCATCHER_DOMAIN env. variable set?');
+
+        // Attempt to guess domain based on PWD + OLDPWD
+        mailcatcher_domain = process.env.PWD + process.env.OLDPWD;
+        if ( mailcatcher_domain.indexOf( '/home/' + process.env.USER + '/web/' ) != -1 ) {
+          String.prototype.delLeftMost = function (sFind) {
+            for (var i = 0; i < this.length; i = i + 1) {
+                var f = this.indexOf(sFind, i);
+                if (f != -1) {
+                    return this.substring(f + sFind.length, f + sFind.length + this.length);
+                    break;
+                }
+            }
+            return this;
+          };
+          String.prototype.getLeftMost = function (sFind) {
+            for (var i = 0; i < this.length; i = i + 1) {
+                var f = this.indexOf(sFind, i);
+                if (f != -1) {
+                    return this.substring(0, f);
+                    break;
+                }
+            }
+            return this;
+          };
+          mailcatcher_domain = mailcatcher_domain.delLeftMost( '/home/' + process.env.USER + '/web/' ).getLeftMost( '/' );
+          smtp_file = '/home/' + process.env.USER + '/web/' + mailcatcher_domain + '/private/smtp.json';
+          if ( fs.existsSync(smtp_file) ) {
+              let smtp_cred = JSON.parse(fs.readFileSync(smtp_file, 'utf8'));
+              mailcatcher_domain = smtp_cred.username;
+              mailcatcher_pw = smtp_cred.password;
+          }else{
+            console.log('SMTP credentials file not found: ' + smtp_file + ' is MAILCATCHER_DOMAIN env. variable set?');
+          }
+        }else{
+          console.log('SMTP credentials file not found: ' + smtp_file + ' is MAILCATCHER_DOMAIN env. variable set?');
+        }
     }
     var smtpOptions = {
         port: this.option('port'),
